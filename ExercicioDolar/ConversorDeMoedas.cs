3c40ad3a7e4ba18344fd80ccaadb5cdf1a92ac2f
Mauricio Costa <mauriciocostaemail@gmail.com>
2019-10-18 13:28:00 -0300

    Apenas teste

---
+++ ExercicioDolar/ConversorDeMoedas.cs
@@ -0,0 +1,7 @@
+﻿namespace ExercicioDolar {
+    class ConversorDeMoedas {
+        public double Conversao(double valorDolar, double qtdeDolar) {
+            return valorDolar * qtdeDolar + (valorDolar * qtdeDolar * 0.06);
+        }
+    }
+}
---
+++ ExercicioDolar/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDolar {
+    class Program {
+        static void Main(string[] args) {
+            ConversorDeMoedas cm = new ConversorDeMoedas();
+
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qtdeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor pago em reais: " + cm.Conversao(cotacaoDolar, qtdeDolar).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
